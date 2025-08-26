7642dab26d60f6f5871fa267b3290c05df7ef9ee
Nícolas Francisco Martins Chagas <125323611+Nickfmc07@users.noreply.github.com>
2025-08-26 18:33:10 -0300

    Alterações na cifra de Hill

--- scripts/geral.js
+++ scripts/geral.js
@@ -1,5 +1,6 @@
+// Aplica temas dinamicamente, adicionando classes às divs com a classe "container"
 const containers = document.querySelectorAll(".container");
-/*
+/* O tema é definido com base na preferência do usuário
 let tema = window.matchMedia('(prefers-color-scheme: dark)').matches
   ? "tema-escuro"
   : "tema-claro";
@@ -9,6 +10,9 @@ containers.forEach(container => {
   container.classList.add(tema);
 });
 
+// Botões de dropdown
+// Adiciona a classe "show" ao conteúdo do dropdown quando o botão é clicado
+// Também inverte a seta do botão ao alterar a classe flipped
 const dropdown = document.querySelectorAll(".dropdown-btn");
 
 dropdown.forEach((btn) => {
@@ -19,3 +23,11 @@ dropdown.forEach((btn) => {
     btn.lastElementChild.classList.toggle("flipped");
   });
 });
+
+// Botão de download para baixar o conteúdo da página como pdf
+const downloadButton = document.getElementById("download-button");
+if (downloadButton) {
+  downloadButton.addEventListener("click", () => {
+    window.print()
+  }
+)}
--- scripts/hill.js
+++ scripts/hill.js
@@ -1,22 +1,33 @@
-const textMatrix1El = document.getElementById('text-message-1')
+const textMatrixEl = document.getElementById('text-message-1')
 const textMatrix2El = document.getElementById('text-message-2')
 
-const numericMatrix1El = document.getElementById('numeric-message-1')
-const numericMatrix2El = document.getElementById('numeric-message-2')
-const numericMatrix3El = document.getElementById('numeric-message-3')
+const numericMatrixEl = document.getElementById('numeric-message-1')
 
 const plainTextInputEl = document.getElementById('plainText')
 
-const keyMatrix1El = document.getElementById('key-matrix-1')
-const keyMatrix2El = document.getElementById('key-matrix-2')
+const keyMatrixEl = document.getElementById('key-matrix-1')
+
+const determinantExpressionEl = document.getElementById('determinant-expression')
 
 const numericEncryptedMatrix1El = document.getElementById('numeric-encrypted-1')
 const numericEncryptedMatrix2El = document.getElementById('numeric-encrypted-2')
 
 const textEncryptedMatrix1El = document.getElementById('text-encrypted-1')
 
-const invKeyMatrix1El = document.getElementById('inv-key-matrix-1')
-const invKeyMatrix2El = document.getElementById('inv-key-matrix-2')
+const invKeyMatrixEl = document.getElementById('inv-key-matrix')
+
+const encryptingProcessEl = document.getElementById('encrypting-process')
+const decryptingProcessEl = document.getElementById('decrypting-process')
+
+const conversionProcess1El = document.getElementById('conversion-process-1')
+const conversionText1El = document.createElement('p')
+conversionText1El.innerHTML = 'As colunas resultantes são então usadas para montar a matriz numérica cifrada, que é convertida de volta para texto. Está é a mensagem cifrada:'
+conversionText1El.id = 'conversion-text-1'
+
+const conversionProcess2El = document.getElementById('conversion-process-2')
+const conversionText2El = document.createElement('p')
+conversionText2El.innerHTML = 'Ao juntar as colunas resultantes, obtemos a matriz decifrada. Ao convertê-la para texto novamente, recuperamos a mensagem original.'
+conversionText2El.id = 'conversion-text-2'
 
 const inputs = document.querySelectorAll('input')
 
@@ -31,6 +42,14 @@ alphabetMap.set(' ', 26)
 alphabetMap.set(26, ' ')
 alphabetMap.set('.', 27)
 alphabetMap.set(27, '.')
+
+const mod = 28 // Tamanho do alfabeto (a-z, espaço, ponto)
+
+function createElementMatrix() {
+    let matrixEl = document.createElement('div')
+    matrixEl.className = 'matrix'
+    return matrixEl
+}
 
 function textToLetterMatrix(text) {
     let textMatrix = [[], []]
@@ -67,26 +86,57 @@ function numericMatrixToTextMatrix(numericMatrix) {
     return letterMatrix;
 }
 
+function assembleHillEncryption(column, keyMatrix, result) {
+    let containerEl = document.createElement('div')
+    containerEl.className = 'rel'
+
+    let columnEl = document.createElement('div')
+    columnEl.className = 'matrix'
+    fillMatrix(columnEl, column)
+
+    let keyEl = document.createElement('div')
+    keyEl.className = 'matrix'
+    fillMatrix(keyEl, keyMatrix)
+
+    let resultEl = document.createElement('div')
+    resultEl.className = 'matrix'
+    fillMatrix(resultEl, result)
+
+    let timesSignEl = document.createElement('img')
+    timesSignEl.src = '../imgs/cross-small.png'
+    timesSignEl.className = 'icon-png'
+
+    let equalSignEl = document.createElement('img')
+    equalSignEl.src = '../imgs/equals.png'
+    equalSignEl.className = 'icon-png'
+
+    containerEl.appendChild(columnEl)
+    containerEl.appendChild(timesSignEl)
+    containerEl.appendChild(keyEl)
+    containerEl.appendChild(equalSignEl)
+    containerEl.appendChild(resultEl)
+
+    return containerEl
+}
 
 function applyHillCipher(numericMatrix, keyMatrix) {
     keyMatrix = math.matrix(keyMatrix)
     let outputMatrix = [[], []]
 
     for (let col = 0; col < numericMatrix[0].length; col++) {
-        let block = [
+        let column = [
             [numericMatrix[0][col]],
             [numericMatrix[1][col]]
         ]
 
-        let outputBlock = math.multiply(keyMatrix, block).toArray()
+        let outputColumn = math.multiply(keyMatrix, column).toArray()
 
         for (let i = 0; i < 2; i++)
-                outputBlock[i][0] = math.mod(outputBlock[i][0], 28);
-        
-        outputMatrix[0].push(...outputBlock[0]);
-        outputMatrix[1].push(...outputBlock[1]);
+            outputColumn[i][0] = math.mod(outputColumn[i][0], 28);
+
+        outputMatrix[0].push(...outputColumn[0]);
+        outputMatrix[1].push(...outputColumn[1]);
     }
-    console.log(outputMatrix)
     return outputMatrix
 }
 
@@ -95,7 +145,7 @@ function encrypt(message, keyMatrix) {
 }
 
 function decrypt(message, inverseKeyMatrix) {
-  return applyHillCipher(message, inverseKeyMatrix);
+    return applyHillCipher(message, inverseKeyMatrix);
 }
 
 function fillMatrix(matrixEl, matrix) {
@@ -124,72 +174,97 @@ function fillMatrix(matrixEl, matrix) {
     });
 }
 
+function getMatrixFromElement(matrixEl) {
+    let matrix = [[], []];
+    for (let i = 0; i < 2; i++)
+        for (let j = 0; j < 2; j++)
+            matrix[i][j] = matrixEl.children[i * 2 + j].value
+    return matrix;
+}
+
 function clearMatrix(matrixEl) {
     matrixEl.innerHTML = '';
 }
 
-function completeKey(keyMatrixEl) {
+function checkCoprimeNumbers(a, b) {
+    while (b !== 0) {
+        rest = a % b;
+        a = b;
+        b = rest;
+    }
+    return a === 1
+}
+
+function findMissingKeyElement(a, b, c) {
+    let aInv = math.invmod(a, mod)
+    let d = math.mod((b * c + 1) * aInv, mod)
+    return d
+}
+
+function completeKey(keyMatrixEl, d) {
     // key = [a b]
     //       [c d]
     let a = keyMatrixEl.children[0].value
     let b = keyMatrixEl.children[1].value
     let c = keyMatrixEl.children[2].value
 
-    let mod = 28
-
-    let aInv = math.invmod(a, mod)
-
-    d = math.mod((b * c + 1) * aInv, mod)
+    d = findMissingKeyElement(a, b, c)
 
     keyMatrixEl.children[3].value = d
 }
 
-function getKeyMatrix(keyMatrixEl) {
-    let matrix = [[], []]; // 2x2 matrix
-    for (let i = 0; i < 2; i++) {
-        for (let j = 0; j < 2; j++) {
-            const index = i * 2 + j
-            const element = parseFloat(keyMatrixEl.children[index].value)
-            matrix[i][j] = isNaN(element) ? 0 : element
-        }
-    }
-    return matrix;
+function assembleDeterminantExpressionLatex(matrix) {
+    let a = matrix[0][0]
+    let b = matrix[0][1]
+    let c = matrix[1][0]
+    let d = matrix[1][1]
+
+    let determinant = math.det(matrix)
+
+    return `Cálculo do determinante: \\[(${a} \\times ${d}) - (${b} \\times ${c}) = ${determinant}\\]`
 }
 
 function clearAllMatrixes() {
-    clearMatrix(document.getElementById('text-message-1'))
-    clearMatrix(document.getElementById('text-message-2'))
-
-    clearMatrix(document.getElementById('numeric-message-1'))
-    clearMatrix(document.getElementById('numeric-message-2'))
-    clearMatrix(document.getElementById('numeric-message-3'))
-
-    clearMatrix(document.getElementById('plainText'))
-
-    clearMatrix(document.getElementById('key-matrix-2'))
-
-    clearMatrix(document.getElementById('numeric-encrypted-1'))
-    clearMatrix(document.getElementById('numeric-encrypted-2'))
-
-    clearMatrix(document.getElementById('text-encrypted-1'))
-
-    clearMatrix(document.getElementById('inv-key-matrix-1'))
-    clearMatrix(document.getElementById('inv-key-matrix-2'))
+    clearMatrix(textMatrixEl)
+    clearMatrix(numericMatrixEl)
+    clearMatrix(plainTextInputEl)
+    clearMatrix(invKeyMatrixEl)
 }
 
-function fillInteractiveContent() {
+function cleanAllProcesses() {
+    // clearing previous content
+    encryptingProcessEl.innerHTML = ''
+    conversionProcess1El.innerHTML = ''
+    decryptingProcessEl.innerHTML = ''
+    conversionProcess2El.innerHTML = ''
     clearAllMatrixes()
 
+    // checking if any of the key matrix inputs are empty, in which case the function returns and does not proceed with the encryption/decryption process
     for (let i = 0; i < 3; i++)
-        if (keyMatrix1El.children[i].value === '') {
-            keyMatrix1El.children[3].value = ''
+        if (keyMatrixEl.children[i].value === '') {
+            keyMatrixEl.children[3].value = ''
             return
         }
 
-    // first step - ecrypting
-    completeKey(keyMatrix1El)
+    // removing previous conversion texts, which are situated outside the interactive content divs that contain conversionProcess1El and conversionProcess2El
+    let pEl
+    pEl = document.getElementById('conversion-text-1')
+    if (pEl) pEl.remove()
+    pEl = document.getElementById('conversion-text-2')
+    if (pEl) pEl.remove()
+}
 
+function fillInteractiveContent() {
+    cleanAllProcesses()
+    // first step - ecrypting
+    if (!checkCoprimeNumbers(keyMatrixEl.children[0].value, mod))
+        return
+
+    completeKey(keyMatrixEl)
     if (plainTextInputEl.value == '') return
+
+    determinantExpressionEl.innerHTML = assembleDeterminantExpressionLatex(getMatrixFromElement(keyMatrixEl))
+    MathJax.typesetPromise([determinantExpressionEl]);
 
     let plainText = plainTextInputEl.value
     plainText = plainText.toLowerCase()
@@ -198,32 +273,74 @@ function fillInteractiveContent() {
     // second step - ecrypting
     let numericMatrix = textMatrixToNumericMatrix(textMatrix)
 
-    fillMatrix(textMatrix1El, textMatrix)
-    fillMatrix(numericMatrix1El, numericMatrix)
+    fillMatrix(textMatrixEl, textMatrix)
+    fillMatrix(numericMatrixEl, numericMatrix)
 
     // third step - ecrypting
-    let keyMatrix = getKeyMatrix(keyMatrix1El)
-    fillMatrix(numericMatrix2El, numericMatrix)
-    fillMatrix(keyMatrix2El, keyMatrix)
-    let numericEncryptedMatrix = encrypt(numericMatrix, keyMatrix)
-    fillMatrix(numericEncryptedMatrix1El, numericEncryptedMatrix)
-    let textEncryptedMatrix = numericMatrixToTextMatrix(numericEncryptedMatrix)
-    fillMatrix(textEncryptedMatrix1El, textEncryptedMatrix)
+    let keyMatrix = getMatrixFromElement(keyMatrixEl)
 
-    //first step - decrypting
+    for (let i = 0; i < numericMatrix[0].length; i++) {
+        let column = [
+            [numericMatrix[0][i]],
+            [numericMatrix[1][i]]
+        ]
+        let result = applyHillCipher(column, keyMatrix)
+        encryptingProcessEl.appendChild(assembleHillEncryption(column, keyMatrix, result))
+    }
+
+    // filler line
+    encryptingProcessEl.appendChild(document.createElement('br'))
+
+    // converting the result to text
+    let encryptedNumericMatrix = applyHillCipher(numericMatrix, keyMatrix)
+    let encryptedTextMatrix = numericMatrixToTextMatrix(encryptedNumericMatrix)
+
+    let encryptedNumericMatrixEl = createElementMatrix()
+    fillMatrix(encryptedNumericMatrixEl, encryptedNumericMatrix)
+    let encryptedTextMatrixEl = createElementMatrix()
+    fillMatrix(encryptedTextMatrixEl, encryptedTextMatrix)
+
+    let arrowSignEl = document.createElement('img')
+    arrowSignEl.src = '../imgs/arrow-right.png'
+    arrowSignEl.className = 'icon-png'
+
+    conversionProcess1El.appendChild(encryptedNumericMatrixEl)
+    conversionProcess1El.appendChild(arrowSignEl)
+    conversionProcess1El.appendChild(encryptedTextMatrixEl)
+
+    conversionProcess1El.parentNode.insertBefore(conversionText1El, conversionProcess1El)
+
+    let textEncryptedMatrix = numericMatrixToTextMatrix(encryptedNumericMatrix)
+
+    //first step - decrypting - calculating inverse key matrix
     let invKeyMatrix = math.inv(keyMatrix)
-    fillMatrix(invKeyMatrix1El, invKeyMatrix)
+    fillMatrix(invKeyMatrixEl, invKeyMatrix)
 
-    //second step - decrypting
-    let numericDecryptedMatrix = decrypt(numericEncryptedMatrix, invKeyMatrix)
-    fillMatrix(numericEncryptedMatrix2El, numericEncryptedMatrix)
-    fillMatrix(invKeyMatrix2El, invKeyMatrix)
-    fillMatrix(numericMatrix3El, numericDecryptedMatrix)
-    fillMatrix(textMatrix2El, numericMatrixToTextMatrix(numericDecryptedMatrix))
+    //second step - decrypting - decrypting the message
+    for (let i = 0; i < encryptedNumericMatrix[0].length; i++) {
+        let column = [
+            [encryptedNumericMatrix[0][i]],
+            [encryptedNumericMatrix[1][i]]
+        ]
+        let result = applyHillCipher(column, invKeyMatrix)
+        decryptingProcessEl.appendChild(assembleHillEncryption(column, invKeyMatrix, result))
+    }
+
+    // converting the result to text
+    let numericDecryptedMatrix = decrypt(encryptedNumericMatrix, invKeyMatrix)
+    let numericDecryptedMatrixEl = createElementMatrix()
+    fillMatrix(numericDecryptedMatrixEl, numericDecryptedMatrix)
+
+    conversionProcess2El.parentNode.insertBefore(conversionText2El, conversionProcess2El)
+    conversionProcess2El.appendChild(numericDecryptedMatrixEl)
+    conversionProcess2El.appendChild(arrowSignEl.cloneNode())
+    conversionProcess2El.appendChild(textMatrixEl.cloneNode(true))
 }
 
 inputs.forEach(input => {
-  input.addEventListener('input', () => {
-    fillInteractiveContent()
-  });
+    input.addEventListener('input', () => {
+        fillInteractiveContent()
+    });
 });
+
+fillInteractiveContent()
